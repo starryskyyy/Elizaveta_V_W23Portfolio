bdc1f332e4598c4e7a9629b7a70a8523b424d773
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-15 18:18:53 -0400

    added cover letter

--- src/components/about/professional-info/Resume.js
+++ src/components/about/professional-info/Resume.js
@@ -15,6 +15,12 @@ export default function Resume() {
                     <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
                     <a href={require("../../../assets/files/Resume.pdf")} download="Resume-Elizaveta-Vygovskaia"><span className="text-white">download</span> {'//'} auto download</a>
                 </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/cover-letter.pdf")} target='_blank' rel="noreferrer"><span className="text-white">cover letter</span> {'//'} open in a new tab</a>
+                </div>
+                <br></br>
             </div>
         </div>
     )
