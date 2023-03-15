108ab385c63f75a9e12fbda100bb2c76909f5766
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-15 16:19:32 -0400

    updated capstone

--- src/components/about/professional-info/Capstone.js
+++ src/components/about/professional-info/Capstone.js
@@ -8,13 +8,44 @@ export default function Capstone() {
             <div className="text-secondary-1 text-base p-10">
                 <div className="flex items-center pl-6 pb-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
-                    <a href={require("../../../assets/files/Resume.pdf")} target='_blank' rel="noreferrer"><span className="text-white">view</span> {'//'} open in a new tab</a>
+                    <a href={require("../../../assets/files/F22_T11_ProjectSummary.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Project Summary</span></a>
                 </div>
                 <br></br>
                 <div className="flex items-center pl-6 pb-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
-                    <a href={require("../../../assets/files/Resume.pdf")} download="Resume-Elizaveta-Vygovskaia"><span className="text-white">download</span> {'//'} auto download</a>
+                    <a href={require("../../../assets/files/F22_T11_ProjectVision.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Project Vision</span></a>
                 </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/F22_T11_HighLevelRequirements.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Project/Business Requirements</span></a>
+                </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/W23_T11_ProjectPlan.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Project Plan</span></a>
+                </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/F22_T11_Requirements_Analysis_and_Design.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Requirements Analysis and Design </span></a>
+                </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/DoForYou.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Wireframes/Mock ups </span>{'//'} web</a>
+                </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/DoForYou_mobile.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Wireframes/Mock ups </span>{'//'} mobile</a>
+                </div>
+                <br></br>
+                <div className="flex items-center pl-6 pb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" fill="rgba(96,123,150,1)" /></svg>
+                    <a href={require("../../../assets/files/W23_T11_ProjectReport1.pdf")} target='_blank' rel="noreferrer"><span className="text-white">Status Report</span></a>
+                </div>
+                <br></br>
             </div>
         </div>
     )
